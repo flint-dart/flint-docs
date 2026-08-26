8776a16fae03f67dd12338af8adaad4855f1e324
unknown <hybiekay2@gmail.com>
2026-08-26 17:03:09 +0100

    build(assets): sync fullstack page chunks and service worker

--- public/flint-sw.js
+++ public/flint-sw.js
@@ -1,4 +1,4 @@
-const FLINT_CACHE = 'flint-ui-1787760109025';
+const FLINT_CACHE = 'flint-ui-1787760176419';
 const FLINT_MANIFEST_URL = '/assets/js/flint-ui/manifest.json';
 
 async function flintCacheUrls(urls) {
